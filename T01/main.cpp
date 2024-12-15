3ad44f360fded17e4d09036178d9e56fbd64ee1d
Denis Komarov <amaerto@ya.ru>
2024-12-15 23:24:55 +0300

    initial functions into namespaces `standart` and `func`, added functions: clear(), showHelp(); initial data structure

--- T01/main.cpp
+++ T01/main.cpp
@@ -1,130 +1,67 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <cstdlib>
 #include <vector>
-
-// INIT
-bool add_data();
-bool show_data();
-bool processCommandChain(std::vector<std::string>& _cmd);
+#include <fstream>
 
 
-struct data
+namespace standart // standart for boot
 {
-	std::string name;
-	std::string surnmae;
-	int bdate[3] = {1, 1, 2024};
-};
+	bool setProcces(); // for command input
+	bool _programm_information(); // after start show programm information
+	bool fileexist(std::fstream &file); // check file exist
+	bool clear(); // terminal clearning
+	bool exit(); // stop programm
+}
 
-
-
-int main (int argc, char* argv[])
+namespace func // functional space
 {
+	struct data // personal data structure
+	{
+		std::string name;
+		std::string surname;
+		std::string date;
+		int pay;
+	};
+
+	bool showHelp(); // show all commands description
+	bool add(); // add new data into file
+	bool showData(); // read file with data and print into terminal
+}
+
+bool func::showHelp()
+{
+	std::cout	<< "\nCOMMAND\tDESCRIPTION" << std::endl
+				<< "-----------------------------------" << std::endl
+				<< "clear\t- clear terminal" << std::endl
+				<< "exit\t- stop programm and exit" << std::endl
+				<< std::endl
+				<< "add\t- add new data" << std::endl
+				<< "list\t- show data list" << std::endl
+				<< std::endl;
 	
-	std::vector<std::string>	__cmd;
-	int							idx = 1;
-	std::string value;
-	while (idx < argc)
-	{
-		value = *(argv + idx);
-		//std::cout << idx << '\t' << *(argv + idx) << std::endl;
-		__cmd.push_back(value);
-		//std::cout << idx << '\t' << value << '\t' << value.length() << std::endl;
-		idx++;
-	}
+	return true;
+}
 
-	while (true)
-	{
-		
+bool standart::clear()
+{
 
-		idx = 0;
-		while (idx < __cmd.size())
-		{
-			std::cout << "check :: " << __cmd[idx] << std::endl;
-			for (std::string _token : {"stop", "exit"})
-			{
-				if (__cmd[idx] == _token)
-				{
-					std::cout << _token << " [CHECKED]" << std::endl;
-					return 0;
-				}
-			}
-			// if (__cmd[idx] == "exit" || __cmd[idx] == "stop")
-			if (__cmd[idx] == "add")
-				add_data();
-			else if (__cmd[idx] == "list")
-				show_data();
-			else
-				std::cout << "[COMMNAD \'" << __cmd[idx] << "\' NOT FOUND!]" << std::endl;
-			
-			
-			idx++;
-		}
-		__cmd.clear();
-		processCommandChain(__cmd);
-	}
-	#ifdef unix
-	std::cout << "unix OS" << std::endl;
-	system("ls");
-	#elif _WIN32
-	std::cout << "WINDOWS OS" << std::endl;
-	system("DIR");
-	#endif
+#ifdef _WIN32 // for WINDOWS
+	system("cls");
+#elif __unix__ // for UNIX
+	system("clear");
+#endif
+
+	return true;
+}
+
+int main()
+{
+	/**
+	 * 		1	Show programm information
+	 * 		2	Show spacial commands `exit` and `clear`
+	 * 		-<>- cycle
+	 * 		3	Command request
+	 * 		4	Check command distr.cmd() --> func
+	 */
 	return 0;
-};
-
-bool add_data()
-{
-	std::cout << ">> ADD DATA" << std::endl;
-	return true;
-}
-
-bool show_data()
-{
-	std::cout << ">> SHOW DATA" << std::endl;
-	return true;
-}
-
-
-bool processCommandChain(std::vector<std::string>& _cmd)
-{
-	char _ch;
-	std::string _command;
-	bool _space = false;
-	while (true)
-	{
-		_ch = std::cin.get();
-		if (_ch == ';')
-		{
-			_space = true;
-			if (_command.length() > 0)
-				_cmd.push_back(_command);
-			_command.clear();
-		}
-		else if (_ch == 32)
-		{
-			//std::cout << _command << '\t' << _command.length() << std::endl;
-			_space = true;
-		}
-		else if (_ch == 10)
-		{
-			//std::cout << _command << '\t' << _command.length() << std::endl << "[ENTER]" << std::endl;
-			_cmd.push_back(_command);
-			_command.clear();
-			break;
-		}
-		else if ((_command.length() > 0) && _space)
-		{
-			_space = false;
-			_command += (char)32;
-			_command += (char)_ch;
-		}
-		else
-		{
-			_space = false;
-			_command += (char)_ch;
-		}
-	}
-	return true;
 }
