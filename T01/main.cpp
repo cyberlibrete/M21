44d196471601bdf4f8535817a51cce06a530a1f5
Denis Komarov <amaerto@ya.ru>
2025-02-01 00:09:09 +0300

    overwrite main.cpp of task 1

--- T01/main.cpp
+++ T01/main.cpp
@@ -1,112 +1,35 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <fstream>
 
-
-namespace standart							// standart for boot
+namespace acc
 {
-	bool _programm_information();			// after start show programm information
-	void clear();							// terminal clearning
-	bool _exit();							// stop programm
-}
-
-namespace func								// functional space
-{
-	struct data								// personal data structure
+	// personal data
+	struct personalData
 	{
-		std::string name;
-		std::string surname;
-		std::string date;
-		int pay;
+		char			_CHECK_STATUS = 0x00;
+		std::string		name;
+		std::string		surname;
+		std::string		issueDate;
+		int				amount;
+
+		personalData();
+		personalData(const char* _name, const char* _surname, const char* _issueDate, int _amount);
 	};
-
-	std::vector<func::data> dataList;
-
-	bool showHelp();						// show all commands description
-	bool add();								// add new data into file
-	bool showData();						// read file with data and print into terminal
 }
 
-bool func::showHelp()
+int main ()
 {
-	std::cout	<< "\nCOMMAND\tDESCRIPTION" << std::endl
-				<< "-----------------------------------" << std::endl
-				<< "clear\t- Clear the terminal screen" << std::endl
-				<< "exit\t- Exit the shell" << std::endl
-				<< "help\t- Display information about builtin commands" << std::endl
-				<< std::endl
-				<< "add\t- Add new data" << std::endl
-				<< "list\t- Show data list" << std::endl
-				<< std::endl;
+	std::cout   << "| MODULE 21 | TASK 01" << std::endl
+				<< "| Accounting statement" << std::endl;
 	
-	return true;
-}
 
-void standart::clear()
-{
-#ifdef _WIN32								// for WINDOWS
-	system("cls");
-#elif __unix__								// for UNIX
-	system("clear");
-#endif
-}
-
-
-// ----------------------------------------------------------------------------------------------------- //
-int main()
-{
-	bool FILE_EXISTS = false;
-	const char* _FILEWITHDATAPATH = "data.txt";
-	/**
-	 * 		1	Show programm information
-	 * 		2	Show spacial commands `exit` and `clear`
-	 * 		-<>- cycle
-	 * 		3	Command request
-	 * 		4	Check command distr.cmd() --> func
-	 */
-
-	std::fstream dataFile(_FILEWITHDATAPATH, std::ios::in);
-	FILE_EXISTS = dataFile.is_open();
-
-
-	std::string _command;
-	standart::clear();
-	standart::_programm_information();
-
-	// 1. print programm information
-	while (true)
-	{
-		// get command | input command
-		std::cout	<< "_$> ";
-		
-		std::getline(std::cin, _command);
-
-		if (_command == "exit")
-			break;
-		else if (_command == "clear")
-			standart::clear();
-		else if (_command == "help")
-			func::showHelp();
-		//else if (_command == "add")
-			//func::add();
-		else if (FILE_EXISTS && (_command == "list"))
-			func::showData();
-		else
-			std::cout << "[Command not found. Enter \"help\" to print available command list]\n";
-	}
 	return 0;
 }
 
-// ===================================================================================================== //
+// -----------------------------------------------------------
+// namespace: acc
 
-bool standart::_programm_information()
+acc::personalData::personalData()
 {
-
-	std::cout	<< "Track payments" << std::endl
-				<< "Version 0.1.0" << std::endl
-				<< "Code on GitHub: https://github.com/cyberlibrete/M21" << std::endl;
-
-
-	return true;
+	this->issueDate = "01.01.2010";
 }
